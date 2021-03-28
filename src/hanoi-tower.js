aab2e30973ccdb2538f6697059f249f0dcf1d228
prig25 <5446068@gmail.com>
2021-03-29 02:46:51 +0300

    feat: implement function

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let n = 0;
+  let t = 0;
+//  var object = {};
+  n = Math.pow(2,disksNumber)-1;
+  t = Math.floor((3600/turnsSpeed)*n);
+/*   object = Object.create({}, { 
+    turns: { value: n },
+    seconds: { value: t }
+  }); */
+  return {turns: n, seconds: t};
+}
+
+//calculateHanoi(5, 4074);
+//{turns: 31, seconds: 27}
