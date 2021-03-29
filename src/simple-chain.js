30f4b2f99aea3289fde78f3c728a12ba85178b97
prig25 <5446068@gmail.com>
2021-03-30 02:01:50 +0300

    feat: implement function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,26 +1,52 @@
 const CustomError = require("../extensions/custom-error");
 
+let arr = [];
+
 const chainMaker = {
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return arr.length;
   },
+
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    arr.push(String(value));
+    return chainMaker;
   },
+
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(!isNaN(position) || (position>0 && position<arr.length)) {
+      arr.splice(position-1, 1);
+      return chainMaker;
+    }
+    arr = [];
+    throw Error('Error');
   },
+
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    arr.reverse();
+    return chainMaker;
   },
+
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    for(let i=0; i<arr.length; i++) {
+      arr[i] = `( ${arr[i]} )`;
+      if(i!==arr.length-1) {
+        arr[i] = `${arr[i]}~~`;
+      }
+    }
+    let result = arr.join('');
+    arr = [];
+    return result;
   }
 };
 
-module.exports = chainMaker;
+module.exports = chainMaker; 
+
+
+
+
+//chainMaker.addLink(1).addLink(2).addLink(3).finishChain();
+//'( 1 )~~( 2 )~~( 3 )'
+//chainMaker.addLink(1).addLink(2).removeLink(1).addLink(3).finishChain();
+//'( 2 )~~( 3 )'
+//chainMaker.addLink(1).addLink(2).reverseChain().addLink(3).finishChain();
+//'( 2 )~~( 1 )~~( 3 )'
